62702f2acbec5a6ddd815133fb9523c5b26e5029
nigel3005 <nigel3005@hotmail.nl>
2023-06-01 11:27:31 +0200

    updated params

--- src/Vliegtuig/JuinitTest.java
+++ src/Vliegtuig/JuinitTest.java
@@ -47,22 +47,22 @@ public class JuinitTest {
         Luchthaven schiphol = new Luchthaven();
 
         Vliegtuig v0 = new Vliegtuig(500, Landen.Nederland, 1, true);
-        Vliegtuig v1 = new Vliegtuig(500, Landen.Anders, 40, false);
+        Vliegtuig v1 = new Vliegtuig(500, Landen.Anders, 2, false);
 
-        Vliegtuig v2 = new Vliegtuig(1050, Landen.Nederland, 1, true);
-        Vliegtuig v3 = new Vliegtuig(1050, Landen.Anders, 50, false);
+        Vliegtuig v2 = new Vliegtuig(1000, Landen.Nederland, 2, true);
+        Vliegtuig v3 = new Vliegtuig(1000, Landen.Anders, 1, false);
 
-        Vliegtuig v4 = new Vliegtuig(6500, Landen.Nederland, 1, true);
-        Vliegtuig v5 = new Vliegtuig(6500, Landen.Anders, 60, false);
+        Vliegtuig v4 = new Vliegtuig(6500, Landen.Nederland, 1, false);
+        Vliegtuig v5 = new Vliegtuig(6500, Landen.Anders, 2, true);
 
         assertEquals(1089, schiphol.getPrijsBijLanden(v0), 0.001);
-        assertEquals(150, schiphol.getPrijsBijLanden(v1), 0.001);
+        assertEquals(100, schiphol.getPrijsBijLanden(v1), 0.001);
 
         assertEquals(1573, schiphol.getPrijsBijLanden(v2), 0.001);
-        assertEquals(750, schiphol.getPrijsBijLanden(v3), 0.001);
+        assertEquals(500, schiphol.getPrijsBijLanden(v3), 0.001);
 
-        assertEquals(3993, schiphol.getPrijsBijLanden(v4), 0.001);
-        assertEquals(3750, schiphol.getPrijsBijLanden(v5), 0.001);
+        assertEquals(3025, schiphol.getPrijsBijLanden(v4), 0.001);
+        assertEquals(3300, schiphol.getPrijsBijLanden(v5), 0.001);
     }
 }
 
